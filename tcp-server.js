5ee7f8347822673bd8b84d6a9b2f825cb4ed3099
Omari Mendoza <omari.mendoza@gmail.com>
2017-10-12 08:48:31 -0400

    server updates

--- tcp-server.js
+++ tcp-server.js
@@ -5,24 +5,35 @@ var clients = [];
 
 const server = net.createServer((socket) => {
 
+   //request user input to customize socket.name
    socket.name = socket.remoteAddress + ":" + socket.remotePort
 
    // Welcome messaging
    socket.write('Welcome ' + socket.name + " !\n")
+   broadcast(`${socket.name} has joined the room`)
 
-   clients = [...clients, socket.address()]
+   clients = [...clients, socket]
 
 
 
-    socket.on('connect', () => {
-        console.log('a new user has connected! IP: ' + socket.address() + " || " + socket.remoteAddress)
+    // socket.on('connect', () => {
+    //     console.log('a new user has connected! IP: ' + socket.address() + " || " + socket.remoteAddress)
+    // })
+
+    socket.on('data', (data) => {
+        process.stdout.write(data.toString('utf-8')) //console your own messages? maybe delete
+        broadcast(`${socket.name}>: ${data}`, socket ) //when data comes into the server, send to all users
     })
 
     socket.on('error', () => {
         console.log('an error has occurred.')
     })
 
-    var Broadcast = (message, sender) => {
+    socket.on('end', () => {
+        broadcast(`{$socket.name}> has left the room`)
+    })
+
+    var broadcast = (message, sender) => {
         //for each in set of sockets, emit this message
         clients.map((client) => {
             if (client !== sender) { //dont send to sender
